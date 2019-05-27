dfab59eca468e0a1dc4af69cda02319c68e41011
phamquocviet <phamviet.eisti@gmail.com>
2019-05-27 18:06:55 +0200

    uploadfile

--- DjangoWeb/Interface/views.py
+++ DjangoWeb/Interface/views.py
@@ -7,8 +7,6 @@ from .forms import ContactForm
 from django.core.mail import EmailMessage
 from django.shortcuts import redirect
 from django.http import HttpResponse
-from django.contrib.auth.decorators import user_passes_test
-from django.db.models import Max
 
 # Create your views here.
 @login_required
@@ -22,106 +20,47 @@ def home(request):
 
 @login_required
 def descriptiveStats(request):
+    # https://simpleisbetterthancomplex.com/tutorial/2016/12/06/how-to-create-group-by-queries.html
+            #     City.objects.values('country__name') \
+            # .annotate(country_population=Sum('population')) \
+            # .order_by('-country_population')
+
     query_results=mergedTables.objects.all()
-    df=mergedTables.pdobjects.all().to_dataframe()
-    qs=mergedTables.objects.all().count()
-    numSTU=mergedTables.objects.values('ID_ANO').distinct().count()
-    numENT=mergedTables.objects.values('ENTREPRISE').distinct().count()
-    STUyear=return_distinct_year(df)
-    # STUQtdPerYear=return_distinct_STUQtdPerYear(df)
-    dataGraph=[1000,10,552,2,63,830,10,84,400]
-    context={'query_results':query_results,
-             'NUMBERLINES':qs,
-             'NUMBERSTU':numSTU,
-             'NUMENT':numENT,
-             'STUyear':STUyear, 
-             'DATAGRAPH':dataGraph
-                }
+    # .count()
+    context={'query_results':query_results}
     return render(request, 'descriptiveStats2.html', context)
 
 @login_required
-@user_passes_test(lambda u: u.is_superuser)
 def etl(request):
-    df_list_versions=return_distinct_version(PRG_STUDENT_SITE.pdobjects.all().to_dataframe())
-    max_version=max(df_list_versions)
-    PRG= PRG_STUDENT_SITE.pdobjects.all().to_dataframe()
-    ADR= ADR_STUDENTS.pdobjects.all().to_dataframe()
-    STU= STUDENT_INTERNSHIP.pdobjects.all().to_dataframe()
+    df_new=showMissingValues( PRG_STUDENT_SITE.pdobjects.all().to_dataframe() )
 
-    version_filtered= request.GET.get('version')
+    df1_new=showMissingValues( ADR_STUDENTS.pdobjects.all().to_dataframe() )
 
-    if version_filtered:
-        PRG=PRG [PRG['idCSV']==version_filtered ]
-        ADR=ADR [ADR['idCSV']==version_filtered ]
-        STU=STU [STU['idCSV']==version_filtered ]
-    else :
-        PRG=PRG [PRG['idCSV']==max_version ]
-        ADR=ADR [ADR['idCSV']==max_version ]
-        STU=STU [STU['idCSV']==max_version ]
+    df2_new=showMissingValues( STUDENT_INTERNSHIP.pdobjects.all().to_dataframe() )
 
-    PRGMissing=showMissingValues( PRG )
-    ADRMissing=showMissingValues( ADR )
-    STUMissing=showMissingValues( STU )
-
-    context={'LIST_VERSIONS': df_list_versions,
-             'MAX_VERSION':max_version,
-             'PRG_STUDENT_SITE':PRGMissing.to_dict('split'),
-             'ADR_STUDENTS':ADRMissing.to_dict('split'),
-             'STUDENT_INTERNSHIP':STUMissing.to_dict('split')
+    context={'PRG_STUDENT_SITE':df_new.to_dict('split'),
+             'ADR_STUDENTS':df1_new.to_dict('split'),
+             'STUDENT_INTERNSHIP':df2_new.to_dict('split')
             }
     
     return render(request, 'etl.html', context)
 
 @login_required
-def etl_mergetables(request):    
-    version=int(mergedTables.objects.all().aggregate(Max('idCSV'))['idCSV__max']) + 1
-    description='Delete Null Values'
-    ADR=redefineDFTypes(ADR_STUDENTS.pdobjects.all().to_dataframe())    
-    PRG=redefineDFTypes(PRG_STUDENT_SITE.pdobjects.all().to_dataframe())
-    STU=redefineDFTypes(STUDENT_INTERNSHIP.pdobjects.all().to_dataframe())
-       
-    df=mergeTables(ADR,PRG,STU)
-    df=deleteMissingValues(df)
-    numberlines = df.ID_ANO.count()
-    table = mergedTables.objects
-    #writeDF2Table(df, table, version, description )
-
-    df=showMissingValues( mergedTables.pdobjects.filter(idCSV=version).to_dataframe() )
-    context={'MERGEDTABLES' :df.to_dict('split') ,
-             'NUMBERLINES'  :numberlines ,
-             'VERSION'      :str(version) + " - " + description
+def etl_mergetables(request):
+    context=mergeTables(
+        ADR_STUDENTS.pdobjects.all().to_dataframe() ,
+        PRG_STUDENT_SITE.pdobjects.all().to_dataframe() ,
+        STUDENT_INTERNSHIP.pdobjects.all().to_dataframe()
+    )
+    df=showMissingValues( mergedTables.pdobjects.all().to_dataframe() )
+    context={'MERGEDTABLES':df.to_dict('split')
             }
     return render(request, 'etl_mergedtables.html', context)
-
-@login_required
-def etl_mergetablesRF(request):    
-    version=int(mergedTables.objects.all().aggregate(Max('idCSV'))['idCSV__max']) + 1
-    description='Fill with RandomForest Algorithm'
-    ADR=redefineDFTypes(ADR_STUDENTS.pdobjects.all().to_dataframe())    
-    PRG=redefineDFTypes(PRG_STUDENT_SITE.pdobjects.all().to_dataframe())
-    STU=redefineDFTypes(STUDENT_INTERNSHIP.pdobjects.all().to_dataframe())
-    LOC=redefineDFTypes(ADR_LOCATION.pdobjects.all().to_dataframe())
-       
-    ADR1,PRG1,STU1=UpdateMissingValues(ADR,PRG,STU,LOC )
-    df=mergeTables(ADR1,PRG1,STU1)
-    print(df)
-    numberlines = df.ID_ANO.count()
-    table = mergedTables.objects
-    #writeDF2Table(df, table, version, description )
-
-    df=showMissingValues( mergedTables.pdobjects.filter(idCSV=version).to_dataframe() )
-    context={'MERGEDTABLES' :df.to_dict('split') ,
-             'NUMBERLINES'  :numberlines ,
-             'VERSION'      :str(version) + " - " + description
-            }
-    return render(request, 'etl_mergedtables.html', context)
-
 
 @login_required
 def maps(request):
     return render(request, 'maps.html')
 
-@login_required
 def contact_us(request):
     form_class = ContactForm
     if request.method == 'POST':
@@ -150,96 +89,48 @@ def contact_us(request):
                 headers = {'Reply-To': contact_email }
             )
             email.send()
-            return redirect('home')
+            return redirect('contact_us')
 
     return render(request, 'contact_us.html', {
         'form': form_class,
     })
 
-#indu 17-may
-def is_valid_queryparam(param):
-    if (param!='Choose...'):
-        return(True)
 
-def is_valid_queryparam2(param1,param2):
-    if (param1!='Choose...') and (param2 != 'Choose...'):
-        return(True)
-
-def checking(request):
-    #query_results= mergedTables.objects.all()
-    #query_results=mergeTables.objects.filter(item=item).distinct('ID_ANO')
-    #query_results= mergedTables.objects.raw('SELECT DISTINCT PRG FROM mergedTables')
-
-    # .count()
-    #context = {'query_results': query_results}
-    query_results=mergedTables.objects.all()
-
-    df_new_prg = return_distinct_prg(mergedTables.pdobjects.all().to_dataframe())
-    df_new_ville = return_distinct_ville(mergedTables.pdobjects.all().to_dataframe())
-    df_new_cp = return_distinct_cp(mergedTables.pdobjects.all().to_dataframe())
-    df_new_rem = return_distinct_rem(mergedTables.pdobjects.all().to_dataframe())
-    df_new_year= return_distinct_year(mergedTables.pdobjects.all().to_dataframe())
-
-
-    prg_filtered= request.GET.get('program')
-    #print(prg_filtered)
-    vil_filtered= request.GET.get('ville')
-    #print(vil_filtered)
-    cod_filtered= request.GET.get('code_postal')
-    rem_filtered= request.GET.get('remuneration')
-    #print(rem_filtered)
-    yfr_filtered=request.GET.get('years_from')
-    #print(yfr_filtered)
-    yto_filtered=request.GET.get('years_to')
-    qs= query_results
-    message=''
-
-    if is_valid_queryparam(prg_filtered):
-        qs=qs.filter(PRG=prg_filtered)
-        #qs= qs & query_results.filter(ADR_VILLE='CERGY' )
-        #print(str(qs.query))
-        #print(prg_filtered)
-
-    if is_valid_queryparam(vil_filtered):
-        qs=qs.filter(ADR_VILLE=vil_filtered)
-        print(qs)
-
-    if is_valid_queryparam(cod_filtered):
-        qs=qs.filter(ADR_CP=cod_filtered)
-
-    if is_valid_queryparam(rem_filtered):
-        qs=qs.filter(REMUNERATION=rem_filtered)
-
-    elif is_valid_queryparam2(yfr_filtered,yto_filtered):
-        if (yfr_filtered <= yto_filtered):
-            yfr_filtered=(int)(yfr_filtered)
-            yfr_anotherhalf= yfr_filtered+1
-            yto_filtered=(int)(yto_filtered)
-            yto_anotherhalf = yto_filtered + 1
-            yfr_filtered  =(str)(yfr_filtered)
-            yto_filtered  = (str)(yto_filtered)
-            yfr_anotherhalf = (str)(yfr_anotherhalf)
-            yto_anotherhalf = (str)(yto_anotherhalf)
-            yfr_filtered=yfr_filtered + '/' + (yfr_anotherhalf)
-            yto_filtered=yto_filtered + '/' + (yto_anotherhalf)
-            print('\n')
-            print('helllooooooo')
-            print(yfr_filtered)
-            qs=query_results.filter(ANNEE_SCOLAIRE__gte=yfr_filtered,ANNEE_SCOLAIRE__lte=yto_filtered)
-        else:
-            message="Years From-To not correct"
-
-    #qs = query_results
-
-    context = {'prg': df_new_prg,
-               'ville': df_new_ville,
-               'cp': df_new_cp,
-               'rem': df_new_rem,
-               'year':df_new_year,
-               'message':message,
-               'query_results':qs,
-               }
+# def contact_us(request):
+#     form_class = ContactForm
+#
+#     # new logic!
+#     if request.method == 'POST':
+#         form = form_class(data=request.POST)
+#
+#         if form.is_valid():
+#             contact_name = request.POST.get('contact_name', '')
+#             contact_email = request.POST.get('contact_email', '')
+#             form_content = request.POST.get('content', '')
+#
+#             # Email the profile with the
+#             # contact information
+#             template = get_template('Interface/contact_template.txt')
+#             context = {
+#                 'contact_name': contact_name,
+#                 'contact_email': contact_email,
+#                 'form_content': form_content,
+#                 }
+#             content = template.render(context)
+#
+#             email = EmailMessage(
+#                 "New contact form submission",
+#                 content,
+#                 "Your website" +'',
+#                 ['induraj2020@gmail.com'],
+#                 headers = {'Reply-To': contact_email }
+#             )
+#             email.send()
+#             return redirect('contact_us')
+#
+#     return render(request,'Interface/contact_us.html', {'form': form_class,})
 
 
-    return render(request,'checking.html',context)
-    #return HttpResponse('<h1> hi </h1>')
+
+# def contact_us(request):
+#     return HttpResponse('<h1> hi </h1>')
--- DjangoWeb/UploadingFile/views.py
+++ DjangoWeb/UploadingFile/views.py
@@ -1,4 +1,3 @@
-
 from django.shortcuts import render
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
@@ -11,34 +10,79 @@ import logging
 
 from django.db import models
 from django.db import connection 
-#from .models import PRG_STUDENT_SITE
-from dataCRUD.models import PRG_STUDENT_SITE 
 
+from dataCRUD.models import PRG_STUDENT_SITE 
+from dataCRUD.models import ADR_STUDENTS
+from dataCRUD.models import STUDENT_INTERNSHIP
+
+from .forms import DocumentForm
+
+import csv
+import codecs
 
 def uploadCSV(request):
      #tables=models._meta.db_table
-     tables=connection.introspection.table_names()
+     #tables=connection.introspection.table_names()
+     
+     Models={'PRG_STUDENT_SITE':1,
+             'ADR_STUDENTS':2,
+             'STUDENT_INTERNSHIP':3,
+            }
+     #modelPicked = request.POST.get('tablesDropdown',False) 
+    
      if request.method == 'POST' and request.FILES['myfile']:
         myfile = request.FILES['myfile']
+        if  'tableselected' in request.GET:
+            tablePicked=request.GET.get('tableselected')
+        else:
+            tablePicked=request.GET.get('tableselected','empty')
+            print(tablePicked)
+        #else:
+        #    return render(request,'uploadcsv.html',{'tables': Models})
+
         if not myfile.name.endswith('.csv'):
             messages.error(request,'File is not CSV type')
             return HttpResponseRedirect(reverse('uploadcsv'))
         elif myfile.multiple_chunks():
             messages.error(request,"Uploaded file is too big (%.2f MB)." % (myfile.size/(1000*1000),))
             return HttpResponseRedirect(reverse('uploadcsv'))
+        
+        #modelPicked=request.POST.get('tablesDropdown','')
+        #modelPicked=request.GET['tablesDropdown']
+        for key in Models:
+            if key==modelPicked:
+                print(modelPicked)
+                file_data = myfile.read().decode("utf-8")		
+                lines = file_data.split("\n")
+                #loop over the lines and save them in db. If error , store as string and then display
+                for line in lines:
+                    if line.strip():
+                    #   line = line.strip().strip('\n')
+                        line.strip('\n')
+                        fields = line.split(",")
+                        prg_student_site=Models['modelPicked'](ID_ANO=fields[1],PRG=fields[2],ANNE_SCOLAIRE=fields[3],SITE=fields[4])
+                        try:
+                            prg_student_site.save()
+                        except Exception as e:
+                            messages.error(request,"Unable to upload file. "+repr(e))
 
-        #tableSelected=request.get['tablesDropdown']
-        file_data = myfile.read().decode("utf-8")		
-        lines = file_data.split("\n")
-		#loop over the lines and save them in db. If error , store as string and then display
-        for line in lines:
-            fields = line.split(",")
-            prg_student_site= PRG_STUDENT_SITE(ID_ANO=fields[1],PRG=fields[2],ANNE_SCOLAIRE=fields[3],SITE=fields[4])
-            try:
-                prg_student_site.save()
-            except Exception as e:
-                messages.error(request,"Unable to upload file. "+repr(e))
-           
+
+  #      file_data = myfile.read().decode("utf-8")		
+  #      lines = file_data.split("\n")
+		##loop over the lines and save them in db. If error , store as string and then display
+  #      for line in lines:
+  #          if line.strip():
+  #          #line = line.strip().strip('\n')
+  #              line.strip('\n')
+  #              fields = line.split(",")
+  #              prg_student_site= PRG_STUDENT_SITE(ID_ANO=fields[1],PRG=fields[2],ANNE_SCOLAIRE=fields[3],SITE=fields[4])
+  #              try:
+  #                  prg_student_site.save()
+  #              except Exception as e:
+  #                  messages.error(request,"Unable to upload file. "+repr(e))
+        
+        #save file to server
+        
         fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
         uploaded_file_url = fs.url(filename)
@@ -46,8 +90,119 @@ def uploadCSV(request):
             'uploaded_file_url': uploaded_file_url
         })
 
-        
-     return render(request, 'uploadcsv.html',{'tables': tables})
+     return render(request, 'uploadcsv.html',{'tables': Models})
     
 
+def model_form_upload(request):
+
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            ptable=form.cleaned_data.get('selectedtable')
+            uploadedFile=request.FILES['document']
+
+            fileType=form.cleaned_data('fileFormat')
+
+            #Process the file
+            if uploadedFile.multiple_chunks():
+                messages.error(request,"Uploaded file is too big (%.2f MB)." % (myfile.size/(1000*1000),))
+            
+            #handle_csv_file(uploadedFile,ptable)
+            reader = csv.DictReader(codecs.iterdecode(uploadedFile, 'utf-8'))
+            for row in reader:
+                id_ano          =row['ID_ANO']
+                prg             =row['PRG']
+                anne_scolaire   =row['ANNE_SCOLAIRE']
+                site            =row['SITE']
+                prg_student_site=PRG_STUDENT_SITE(ID_ANO=id_ano,PRG=prg,ANNE_SCOLAIRE=anne_scolaire,SITE=site)
+                try:
+                    prg_student_site.save()
+                except Exception as e:
+                    messages.error(request,"Unable to save csv data to the database. "+repr(e))   
+
+            #if uploadedFile.name.endswith(filetypeSelected):
+            #if uploadedFile.name.endswith(fileType):
+            #    handle_csv_file(uploadedFile,ptable)
+           
+            #elif uploadedFile.name.endswith(filetypeSelected):
+            #    handle_uploaded_file(uploadedFile)
+            #else:
+            #    messages.error(request,'File is not CSV & text type')
+            #    return HttpResponseRedirect(reverse('model_form_upload'))
+
+            form.save()
+            return HttpResponseRedirect(reverse('model_form_upload'))
+    else:
+        form = DocumentForm()
+    return render(request, 'model_form_upload.html',{'form': form})
+
+def handle_uploaded_file(f,table):
+    
+    file_data = f.read().decode("utf-8")		
+    lines = file_data.split("\n")
+    #loop over the lines and save them in db. If error , store as string and then display
+    for line in lines:
+        if line.strip():
+        #   line = line.strip().strip('\n')
+            line.strip('\n')
+            fields = line.split(",")
+            prg_student_site=eval(table)(ID_ANO=fields[1],PRG=fields[2],ANNE_SCOLAIRE=fields[3],SITE=fields[4])
+            try:
+                prg_student_site.save()
+            except Exception as e:
+                messages.error(request,"Unable to upload file. "+repr(e))   
+
+def handle_csv_file(f,tablePicked):
+    reader = csv.DictReader(codecs.iterdecode(f, 'utf-8'))
+    for row in reader:
+        id_ano          =row['ID_ANO']
+        prg             =row['PRG']
+        anne_scolaire   =row['ANNE_SCOLAIRE']
+        site            =row['SITE']
+        prg_student_site=PRG_STUDENT_SITE(ID_ANO=id_ano,PRG=prg,ANNE_SCOLAIRE=anne_scolaire,SITE=site)
+        try:
+            prg_student_site.save()
+        except Exception as e:
+            messages.error(request,"Unable to save csv data to the database. "+repr(e))   
+    #if tablePicked==PRG_STUDENT_SITE:
+    #    for row in reader:
+    #        id_ano          =row['ID_ANO']
+    #        prg             =row['PRG']
+    #        anne_scolaire   =row['ANNE_SCOLAIRE']
+    #        site            =row['SITE']
+    #        prg_student_site=PRG_STUDENT_SITE(ID_ANO=id_ano,PRG=prg,ANNE_SCOLAIRE=anne_scolaire,SITE=site)
+    #        try:
+    #            prg_student_site.save()
+    #        except Exception as e:
+    #            messages.error(request,"Unable to save csv data to the database. "+repr(e))   
+    #if tablePicked==ADR_STUDENTS:
+    #    for row in reader:
+    #        adr_cp      =row['ADR_CP']
+    #        adr_ville   =row['ADR_VILLE']
+    #        adr_pays    =row['ADR_PAYS']
+    #        site        =row['SITE']
+    #        id_ano      =row['ID_ANO']
+    #        adr_student=ADR_STUDENTS(ADR_CP=adr_cp,ADR_VILLE=adr_ville,ADR_PAYS=adr_pays,SITE=site,ID_ANO=id_ano)
+    #        try:
+    #            adr_student.save()
+    #        except Exception as e:
+    #            messages.error(request,"Unable to save csv data to the database. "+repr(e)) 
+    #if tablePicked==STUDENT_INTERNSHIP:
+    #    for row in reader:
+    #        annee=row['ANNEE']
+    #        anne_scolaire=row['ANNE_SCOLAIRE']
+    #        entreprise=row['ENTREPRISE']
+    #        code_postal=row['CODE_POSTAL']
+    #        ville=row['VILLE']
+    #        pay=row['PAY']
+    #        sujet=row['SUJET']
+    #        remuneration=row['REMUNERATION']
+    #        id_ano=row['ID_ANO']
+    #        student_internship=STUDENT_INTERNSHIP(ANNEE=annee,ANNE_SCOLAIRE=anne_scolaire,ENTREPRISE=entreprise,
+    #                                              CODE_POSTAL=code_postal,VILLE=ville,PAY=pay,SUJET=sujet,
+    #                                              REMUNERATION=remuneration,ID_ANO=id_ano,SITE=site)
+    #        try:
+    #            student_internship.save()
+    #        except Exception as e:
+    #            messages.error(request,"Unable to save csv data to the database. "+repr(e)) 
 # Create your views here.
